18e3638d980b0a924b2fa875e0de110d7fff4529
Lizeth <liza_teza94@hotmail.com>
2020-12-28 20:28:47 -0600

    Observable added to mantein data of active user

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,9 +7,27 @@ import {
 } from "react-router-dom";
 import JournalScreen from '../components/journal/JournalScreen';
 import AuthRouter from './AuthRouter';
-
+import { firebase } from '../firebase/firebase-config'
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 const AppRouter = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        //creamos un observable 
+        firebase.auth().onAuthStateChanged((user) => {
+            console.log(user);
+            //verificamos que existe el user y que tenga la prop uid
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName))
+            }//, si es null...
+
+        })
+        //agregamos el dispatch como dep para que deje de mostrar warning
+    }, [dispatch])
+
     return (
         <Router>
             <div>
